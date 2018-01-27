ee91483d359bc3fc8c26c8c27ddac3d3c63d331a
lijianxun <medusa.leee@gmail.com>
2018-01-27 17:05:13 +0800

    完善抽象类示例

--- class/abstract.ts
+++ class/abstract.ts
@@ -1,8 +1,15 @@
-abstract class Ancestor {
+abstract class Human {
+    public sayHello(): void {
+        console.log('Hello...');
+    }
+}
+// 抽象类可以继承抽象类，也可以实现接口
+abstract class Ancestor extends Human{
     // 祖先的姓氏，如果设置为private子类将无法继承此成员变量
     public lastName: string = '李';
 
     constructor() {
+        super();
     }
 
     // 祖先的说姓氏方法
@@ -14,8 +21,8 @@ abstract class Ancestor {
     public abstract sayFullName(): void;
 }
 
-// 继承祖先的姓氏lastName，并实现祖先给的sayName的方法
-class Human extends Ancestor {
+// 继承祖先的姓氏lastName，说姓氏的方法sayLastName，并实现祖先给的sayFullName的方法
+class Man extends Ancestor {
     public firstName: string;
 
     constructor(firstName: string) {
@@ -28,8 +35,30 @@ class Human extends Ancestor {
     }
 }
 
-const human: Human = new Human('建勋');
+// const ancestor: Ancestor = new Ancestor(); // 抽象类不能实例化！！！只能被继承
+let ancestor: Ancestor; // 但是可以声明为变量的类型
+
+// 声明为Man可以调用所有父类和自己中的方法
+const man1: Man = new Man('建勋');
+// 调用人类的的sayHello方法
+man1.sayHello();
 // 调用继承自祖先的说姓氏方法
-human.sayLastName();
+man1.sayLastName();
 // 调用自己实现的说全名方法
-human.sayFullName();
+man1.sayFullName();
+
+// 声明为Ancestor可以调用所有父类和自己中的方法
+const man2: Ancestor = new Man('建勋');
+// 调用人类的的sayHello方法
+man2.sayHello();
+// 调用继承自祖先的说姓氏方法
+man2.sayLastName();
+// 调用自己实现的说全名方法
+man2.sayFullName();
+
+// 声明为Human只能调用Human中的方法，因为Human中没有sayLastName和sayFullName方法
+const man3: Human = new Man('建勋');
+// 调用人类的的sayHello方法
+man3.sayHello();
+// man3.sayLastName(); // 错误
+// man3.sayFullName(); // 错误
